afc9a0fc432e8a13075fba728b581e5eb40cf09d
xaralis <filip.varecha@fragaria.cz>
2019-11-22 16:07:43 +0100

    Fix import err

--- ksicht/bulma/forms.py
+++ ksicht/bulma/forms.py
@@ -1,7 +1,8 @@
 from django.forms import EmailField as DjangoEmailField
 from django.forms import FileField as DjangoFileField
 from django.forms import ImageField as DjangoImageField
-from django_crispy_bulma.widgets import EmailInput, FileUploadInput
+
+from .widgets import EmailInput, FileUploadInput
 
 
 class EmailField(DjangoEmailField):
