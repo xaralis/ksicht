af9ea9f0184d6a9961e6cd2dbf63b08a7528a544
xaralis <filip@varecha.work>
2023-05-14 12:27:52 +0200

    fix: cache /w relation to currently active page

--- ksicht/core/templatetags/pages.py
+++ ksicht/core/templatetags/pages.py
@@ -1,5 +1,6 @@
 from django import template
 from django.contrib.flatpages.models import FlatPage
+from django.core.cache import cache
 
 
 register = template.Library()
@@ -7,9 +8,14 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def pages_by_prefix(context, prefix):
-    matching_pages = FlatPage.objects.filter(url__startswith=prefix).prefetch_related(
-        "metadata__allowed_groups"
-    )
+    matching_pages = cache.get(f"pages_by_prefix::{prefix}")
+
+    if not matching_pages:
+        matching_pages = FlatPage.objects.filter(url__startswith=prefix).prefetch_related(
+            "metadata__allowed_groups"
+        )
+        cache.set(f"pages_by_prefix::{prefix}", matching_pages, 120)
+
     return [
         p
         for p in matching_pages
